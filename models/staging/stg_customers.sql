7dee1915790f1080d68e096b49b482c45cf20309
Damodara Teixeira <damodara.teixeira@indicium.tech>
2024-02-12 03:49:39 +0000

    Add of stg_customer

---
+++ models/staging/stg_customers.sql
@@ -0,0 +1,10 @@
+with
+    customer as (
+        select
+            cast(customerid as int) as customerid
+            , cast(personid as int) as personid
+            , cast(storeid as int) as storeid
+            , cast(territoryid as int) as territoryid
+        from {{ source('adventure_works', 'customer') }}
+    )
+select * from customer
