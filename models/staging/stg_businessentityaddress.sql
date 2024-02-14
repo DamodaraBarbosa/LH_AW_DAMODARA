02a6ef1c7b13ac9413b31c523d9200193c01b8f7
Damodara Teixeira <damodara.teixeira@indicium.tech>
2024-02-14 12:02:50 +0000

    Modified stg_address.yml and created stg_businessentityaddress.sql

---
+++ models/staging/stg_businessentityaddress.sql
@@ -0,0 +1,10 @@
+with
+    businessentityaddress as (
+        select
+        businessentityid
+        , addressid
+        , addresstypeid
+        from {{ source('adventure_works', 'businessentityaddress') }}
+    )
+select *
+from businessentityaddress
