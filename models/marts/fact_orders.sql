520f157d3585559297943095a0d63a23aaa95797
Damodara Teixeira <damodara.teixeira@indicium.tech>
2024-02-27 11:35:35 +0000

    Added case when to status in salesorderheader

--- models/marts/fact_orders.sql
+++ models/marts/fact_orders.sql
@@ -7,7 +7,14 @@ with
             , orderdate
             , duedate
             , shipdate
-            , status
+            , case
+                when status = 1 then "In process"
+                when status = 2 then "Approved"
+                when status = 3 then "Backordered"
+                when status = 4 then "Rejected"
+                when status = 5 then "Shipped"
+                when status = 6 then "Cancelled"
+            end as status
             , onlineorderflag
             , customerid
             , territoryid
